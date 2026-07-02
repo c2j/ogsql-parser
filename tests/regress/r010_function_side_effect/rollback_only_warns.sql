a131e248bb9b9dbf9cb28ad0fa15d870e50c37e2
Jianjun Chen <chenjj.yz@gmail.com>
2026-07-02 11:55:16 +0800

    Revert "test(regress): add new R010 fixtures for DML-ok and tx-only scenarios"
    This reverts commit 404fb568bcc3fae449d978f3c20d116a60ab9340.

--- tests/regress/r010_function_side_effect/rollback_only_warns.sql
+++
@@ -1,9 +0,0 @@
--- description: function with only ROLLBACK (no DML) should trigger R010
--- warn: R010
-CREATE OR REPLACE FUNCTION fn_rollback_only() RETURNS void
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    ROLLBACK;
-END;
-$$;
